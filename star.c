32d26bc6abcc9e0bbde035e2bfcb3156a4bcf822
bamtolee <79846677+bamtolee@users.noreply.github.com>
2021-04-05 02:16:40 +0900

    initial commit

---
+++ star.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main()
+{
+	int i,n,j,s,t,k;
+	scanf("%d",&n);
+	
+	for(i=0;i<n-1;i++)
+	{
+		for(j=n-1;j>i;j--)
+			printf(" ");
+		
+		printf("*");
+		
+		if(i>=1)
+		{
+			for(j=1;j<=2*i-1;j++)
+				printf(" ");
+			printf("*");
+		}		
+	printf("\n");	
+	}
+	
+	for(s=0;s<2*n-1;s++)
+		printf("*");
+		
+	return 0;
+}
